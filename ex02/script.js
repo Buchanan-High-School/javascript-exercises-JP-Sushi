1d743f2398df0dcb93225b619ed34f39eeb89702
Jp <601043@buchananschools.com>
2024-11-15 11:52:08 -0500

    Edted java

--- ex02/script.js
+++ ex02/script.js
@@ -1,14 +1,24 @@
 // Write your Task 1 code inside this function!
 function task1() {
   console.log("Running task 1");
+  let target = document.querySelector("#text-content")
+  console.log(target)
+  target.innerText = "Additinal text into text box"
 }
 
 // Write your task 2 code inside this function!
 function task2() {
   console.log("Running task 2");
+  let target = document.querySelector("#task2")
+  console.log(target)
+  target.classList.toggle("color");
+  console.log(target)
+
 }
 
 // Write your task 3 code inside this function!
 function task3() {
   console.log("Running task 3");
+  let target = document.querySelector("task3")
+  
 }
