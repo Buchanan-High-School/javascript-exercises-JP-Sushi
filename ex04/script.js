6cfc451e5fedb705508365c3adc3fb241137582b
Jp <601043@buchananschools.com>
2024-11-22 12:14:26 -0500

    task 1 completed

--- ex04/script.js
+++ ex04/script.js
@@ -1,18 +1,33 @@
 function task1() {
-  let myArray = [];
+  let myList = ["info", "more info", "even more info", "a lot more info"];
   // This will run when you click the button.
-  myArray.forEach((item) => {
-    let el = document.createElement("p");
+  myList.forEach((item) => {
+    let el = document.createElement("li");
     el.innerText = item;
+    console.log(myList)
+    let target = document.querySelector("div")
     target.appendChild(el);
   });
 }
 
 function task2A() {
+  document.querySelectorAll("color-list")
+let target1 = document.querySelector("#li1")
+let target2 = document.querySelector("#li2")
+let target3 = document.querySelector("#li3")
+let target4 = document.querySelector("#li4")
+let target5 = document.querySelector("#li5")
+let target6 = document.querySelector("#li6")
+console.log(target1, target2, target3, target4, target5, target6)
+  
+  
   // Write your code in here
 }
 
 function task2B() {
+  
+  
+  
   // Writ your code in here
 }
 
