c770dc40cec789809659fa17e485403239d9fd5a
Jp <601043@buchananschools.com>
2024-11-26 11:53:00 -0500

    Completed task three and revised task 2 B

--- ex04/script.js
+++ ex04/script.js
@@ -19,18 +19,35 @@ let target4 = document.querySelector("#li4")
 let target5 = document.querySelector("#li5")
 let target6 = document.querySelector("#li6")
 console.log(target1, target2, target3, target4, target5, target6)
-  
-  
+let color = ["Red", "Orange", "Yellow", "Green", "Blue", "Violet"]
+color.forEach(color => {
+  console.log(color)
+})
   // Write your code in here
 }
 
 function task2B() {
-  
-  
-  
+  document.querySelectorAll("name-list")
+let name = ["Hi Mr. Bennett", "Hi Mrs. Frost", "Hi Mr. Cribley", "Hi Mrs. Lentz", "Hi Mr. Pruett"]
+name.forEach(greet => {
+  console.log(greet)
+})
+let target = document.querySelector("#name-list")
+name.forEach(name => {
+  let el = document.createElement("p")
+  el.innerText = name;
+  target.appendChild(el)
+  console.log(target)
+})
   // Writ your code in here
 }
 
 function task3() {
+ let name = document.querySelectorAll(".item")
+name.forEach(color => {
+  console.log(color)
+  // color.style.backgroundColor = "#" + color.dataset.color;
+  color.style.backgroundColor = "#" + color.innerText
+})
   // Write your code in here
 }
